d5012f54a5ae298aa736e1502482bbae205739b4
Pedro Giovane <157719956+pedrogiovn21@users.noreply.github.com>
2025-03-12 05:48:29 -0300

    Update api.js

--- assets/JS/api.js
+++ assets/JS/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = "https://raw.githubusercontent.com/pedrogiovn21/js-developer-portfolio/refs/heads/main/data/profile.json"
+    const url = "https://raw.githubusercontent.com/pedrogiovn21/curriculoDio.js/refs/heads/main/data/profile.json"
     const fetching = await fetch(url)
     return await fetching.json()
 }
