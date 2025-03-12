88e9a950cadcdd1b8a8b82769a9cbd9b5051c60e
Pedro Giovane <157719956+pedrogiovn21@users.noreply.github.com>
2025-03-12 05:56:23 -0300

    Delete assets directory

--- assets/JS/api.js
+++
@@ -1,5 +0,0 @@
-async function fetchProfileData() {
-    const url = "https://raw.githubusercontent.com/pedrogiovn21/curriculoDio.js/refs/heads/main/data/profile.json"
-    const fetching = await fetch(url)
-    return await fetching.json()
-}
